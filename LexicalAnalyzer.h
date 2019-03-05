de75f04beac6af16fa831dce1f184e1654bfee13
Tenzin Dorjee <tenzindorjee@outlook.com>
2019-03-05 04:49:21 -0800

    Update LexicalAnalyzer.h
    1. had to change vector size because Dynamic 
    2. had to deal with string to char conversions because of issues 
    3. added functions to figure out whether the char was a valid seperator/operator
    4.need to finish the Lexer function ASAP

--- LexicalAnalyzer.h
+++ LexicalAnalyzer.h
@@ -7,6 +7,7 @@
 #include <vector>
 #include <cctype>
 #include <iomanip>
+#include <string>
 using namespace std;
 
 class LexicalAnalyzer
@@ -16,40 +17,21 @@ class LexicalAnalyzer
     string OPERATORS[8] = {"*", "+", "-", "=", "/", ">", "<", "%"};
     string SEPERATORS[13] = {"'", "(", ")", "{", "}", "[", "]", ",", ".", ":", ";", "!", " "};
     string STATE[8] = {"UNKNOWN", "IDENTIFIER", "INTEGER", "KEYWORD", "SEPERATOR", "OPERATOR", "REAL", "DECIMAL"};
+    const int legal_identifier[5][3] = {{2, 5, 5}, {2, 3, 4}, {5, 5, 4}, {5, 4, 5}, {1, 1, 1}}; // legal identifier fsm
+    const int digit_or_float_fsm[4][2] = {{4, 2}, {3, 2}, {4, 2}, {1, 1}};                      // real or float fsm
+    int vectorIndex = 0;                                                                        //contains current index of the vector of chars so we can test which index we are at
+    int currentState = 1;                                                                       // default state
+    bool acceptedOrNot = false;                                                                 //whether the state is accepted or not
+    string textFileHolder;                                                                      //holds the text file into a string to iterate later on
+    vector<char> charVector;
 
-    // list tokenList; // idk yet
-    // list lexemeList;
-    
-    //FSM table for identifiers
-    //inputs: letter, digit, or $
-    const int ID_FSM_States[5][3] = {
-        {2, 5, 5},      //state 1(starting state); only accepts a letter initially, goes to dead state otherwise
-        {2, 3, 4},      //state 2(final state)
-        {5, 5, 4},      //state 3(final state); goes to dead state if input is a letter or digit
-        {5, 4, 5},      //state 4(final state); goes to dead state if input is a letter or $
-        {1, 1, 1}       //state 5(dead state); goes back to starting state
-    };
-    
-    //FSM table for real and floating point numbers
-    //inputs: digit or .
-    const int RF_FSM_States[4][2] = {
-        {2, 4},     //state 1(starting state)
-        {2, 3},     //state 2(final state)
-        {2, 4},     //state 3
-        {1, 1}      //state 4
-    };
-        
-    }
-    int currentState = 1;       // default state
-    bool acceptedOrNot = false; //whether the state is accepted or not
-    string textFileHolder;      //holds the text file into a string to iterate later on
     typedef struct tokens
     {
         string identifierState; // token identifier
         string lexemes;         //lexemes
     } token;
 
-    vector<token> tokenVector[100]; //holds the token
+    vector<token> tokenVector; //holds the token
   public:
     void LexicalAnalyzer::textToString(string textFile)
     { //opens textfile and then stores it into a string for further use
@@ -61,33 +43,66 @@ class LexicalAnalyzer
             exit(1);
         }
         holder >> textFileHolder;
+        string_to_charVector(textFileHolder);
     }
-    void LexicalAnalyzer::printTokenAndLexemes(vectorToken[])
-    {
-        //iterates through the token vector until it reaches max size might need to fix up
-        //Will output into a text file
-        fstream outfile;
-        outfile.open("output.txt");
-        outfile << "TOKENS" << setw(15) << "LEXEMES" << endl;
+    void LexicalAnalyzer::printTokenAndLexemes(vector<token> vectorToken)
+    { //iterates through the token vector until it reaches max size might need to fix up
+        cout << "TOKENS" << setw(15) << "LEXEMES" << endl;
 
-        for (int i = 0; i < vectorToken.size(); i++)
+        for (unsigned int i = 0; i < vectorToken.size(); i++)
         {
-            int tempToken = vectorToken[i].identifierState;
-            int tempLexemes = vectorToken[i].lexemes;
+            string tempToken = vectorToken[i].identifierState;
+            string tempLexemes = vectorToken[i].lexemes;
 
-            outfile << tempToken << setw(13) << "=" << tempLexemes << endl;
+            cout << tempToken << setw(13) << "=" << tempLexemes << endl;
         }
-        outfile.close();
     }
-    // NEED TO WORK ON THIS AND THE TABLE
-    token LexicalAnalyzer::Lexer()
+
+    void LexicalAnalyzer::string_to_charVector(string textFileHolder)
+    { //converts the string that holds all the text file to a char vector for further manipulation
+        for (unsigned int i = 0; i < textFileHolder.length(); i++)
+        {
+            charVector[i] = textFileHolder[i];
+        }
+    }
+
+    token LexicalAnalyzer::Lexer(vector<char> charVector)
+    { //NEED TO NO LIFE THE FUCK OUT OF THIS PART BRUH
+        while (!charVector.empty())
+        {
+        }
+    }
+
+    bool LexicalAnalyzer::isSeperator(char c)
     {
-        
+        string tempString; // a temporary string so that we can convert the char to string for comparison to the seperator array
+        tempString += c;   // tempString = tempString + char
+
+        for (unsigned int i = 0; i < 13; i++)
+        {
+            tempString == SEPERATORS[i];
+            return 1; // returns true if the char/string is equivalent to a valid seperator
+        }
+        return 0;
+    }
+
+    bool LexicalAnalyzer::isOperator(char c)
+    {
+        string tempString;
+        tempString += c;
+
+        for (unsigned int i = 0; i < 8; i++)
+        {
+            tempString == OPERATORS[i];
+            return 1;
+        }
+        return 0;
     }
 
     int LexicalAnalyzer::char_to_column(char c)
     { //functions to find the column the char belongs to so it can go through the table to find if accepted or not
-        if (isalpha(c) || c == '$')
+
+        if (isalpha(c))
         {
             return 1;
         }
@@ -97,7 +112,14 @@ class LexicalAnalyzer
             return 2;
         }
 
-        else if (c==' '){
-            return 
+        else if (c == '$')
+        {
+            return 3;
+        }
+
+        else if (c == '.')
+        {
+            return 1;
         }
     }
+}
